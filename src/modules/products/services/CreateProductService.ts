c01c447113ae15adb97bbb794c76a35469d4287d
Wilson Neto <wilsonpsn@hotmail.com>
2021-02-24 19:40:13 -0300

    Create product service is done

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import RepositoryNames from '@shared/container/RepositoryNames';
 import Product from '../infra/typeorm/entities/Product';
 import IProductsRepository from '../repositories/IProductsRepository';
 
@@ -13,10 +14,27 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject(RepositoryNames.PRODUCTS_REPOSITORY)
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const checkNameExists = await this.productsRepository.findByName(name);
+
+    if (checkNameExists) {
+      throw new AppError('Product with current name already exists');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    console.log("🚀 ~ file: CreateProductService.ts ~ line 34 ~ CreateProductService ~ execute ~ product", product)
+
+    return product;
   }
 }
 
