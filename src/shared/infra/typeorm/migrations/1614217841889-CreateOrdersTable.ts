1fa8b15026af2b669ae1363ba3ac3f83ba20ba82
Wilson Neto <wilsonpsn@hotmail.com>
2021-02-24 23:02:19 -0300

    Create orders table

---
+++ src/shared/infra/typeorm/migrations/1614217841889-CreateOrdersTable.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+const TABLE_NAME = 'orders';
+
+export default class CreateOrdersTable1614217841889
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(
+      new Table({
+        name: TABLE_NAME,
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable(TABLE_NAME);
+  }
+}
